3c466d18cda7affb3272042d43023976027facf0
lazdinst <lazdinst@gmail.com>
2024-12-18 06:43:44 -0500

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,29 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  return search(haystack, 0, haystack.length, needle);
+}
 
+function search(
+  arr: number[],
+  low: number,
+  high: number,
+  needle: number,
+): boolean {
+  while (low < high) {
+    const middle = getMiddle(low, high);
+    const value = arr[middle];
+
+    if (value === needle) {
+      return true;
+    } else if (value > needle) {
+      high = middle;
+    } else {
+      low = middle + 1;
+    }
+  }
+  return false;
+}
+
+function getMiddle(low: number, high: number) {
+  let half = Math.floor(low + (high - low) / 2);
+  return half;
 }
